e54ae5cff73705975c33c0cd3ea5b17326fd5b3b
anwarsha479 <anwarsha4790@gmail.com>
2026-06-25 21:36:40 +0530

    Add Employee Dashboard

--- Backend/src/dashboard/dashboard.controller.ts
+++ Backend/src/dashboard/dashboard.controller.ts
@@ -1,13 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
-
+import {
+  Controller,
+  Get,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { Request } from 'express';
 import { DashboardService } from './dashboard.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('dashboard')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
+
 export class DashboardController {
-  constructor(private readonly dashboardService: DashboardService) {}
+  constructor(
+    private readonly dashboardService: DashboardService,
+  ) {}
 
   @Get('stats')
   getStats() {
     return this.dashboardService.getStats();
+  }
+
+  @Get('employee')
+  getEmployeeStats(
+    @Req()
+    req: Request & {
+      user: {
+        userId: string;
+      };
+    },
+  ) {
+    return this.dashboardService.getEmployeeStats(
+      req.user.userId,
+    );
   }
 }
--- Backend/src/dashboard/dashboard.service.ts
+++ Backend/src/dashboard/dashboard.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@nestjs/common';
-
 import { InjectRepository } from '@nestjs/typeorm';
-
 import { Repository } from 'typeorm';
-
 import { Employee } from '../employees/entities/employee.entity';
 import { Department } from '../departments/entities/department.entity';
 import { Leave } from '../leaves/entities/leave.entity';
@@ -20,15 +17,12 @@ export class DashboardService {
 
     @InjectRepository(Leave)
     private readonly leaveRepository: Repository<Leave>,
-  ) {}
+  ) { }
 
   async getStats() {
     const totalEmployees = await this.employeeRepository.count();
-
     const totalDepartments = await this.departmentRepository.count();
-
     const totalLeaves = await this.leaveRepository.count();
-
     const pendingLeaves = await this.leaveRepository.count({
       where: {
         status: LeaveStatus.PENDING,
@@ -54,6 +48,64 @@ export class DashboardService {
       pendingLeaves,
       approvedLeaves,
       rejectedLeaves,
+    };
+  }
+
+  async getEmployeeStats(userId: string) {
+    const appliedLeaves = await this.leaveRepository.count({
+      where: {
+        employee: {
+          user: {
+            id: userId,
+          },
+        },
+      },
+      relations: {
+        employee: {
+          user: true,
+        },
+      },
+    });
+
+    const approvedLeaves = await this.leaveRepository.count({
+      where: {
+        employee: {
+          user: {
+            id: userId,
+          },
+        },
+        status: LeaveStatus.APPROVED,
+      },
+      relations: {
+        employee: {
+          user: true,
+        },
+      },
+    });
+
+    const rejectedLeaves = await this.leaveRepository.count({
+      where: {
+        employee: {
+          user: {
+            id: userId,
+          },
+        },
+        status: LeaveStatus.REJECTED,
+      },
+      relations: {
+        employee: {
+          user: true,
+        },
+      },
+    });
+
+    const remainingLeaves = 24 - approvedLeaves;
+
+    return {
+      appliedLeaves,
+      approvedLeaves,
+      rejectedLeaves,
+      remainingLeaves,
     };
   }
 }
